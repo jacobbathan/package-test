5520405c0cbd7338646ba480f12e7c2340d6a9a5
jacob <jacobbathan@gmail.com>
2022-01-26 16:25:44 -0800

    added stuff

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 
 "use strict";
 
+import chalk from "chalk";
+
 console.log("THIS LOG IS FROM THE NODE PACKAGE DISREGARD OR CELEBRATE");
 
 process.on("unhandledRejection", (err) => {
+  console.log(chalk.bgRed.bold("ERROR"));
   throw err;
 });
 
 const args = process.argv.slice(2);
 console.log("ARG", args);
+if (!args) {
+  console.log(chalk.bgYellow("NO ARGUMENTS PASSED."));
+}
+
+console.log(chalk.bgBlue("Initializing Test " + args));
