4082e46d53d5a8dc5758baa893b274c0473b8546
jacob <jacobbathan@gmail.com>
2022-01-26 16:14:24 -0800

    update package json, added arguments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+#!/usr/bin/env node
+
+"use strict";
+
 console.log("THIS LOG IS FROM THE NODE PACKAGE DISREGARD OR CELEBRATE");
+
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+
+const args = process.argv.slice(2);
+console.log("ARG", args);
